b5ee28b2910201c07cead2a7535af1809078b285
Roman <scher.roman@gmail.com>
2016-11-30 22:57:48 -0500

    Added proper login redirects, separated view & form endpoints
    Login will redirect you back to the page that requested login, or the
    page you were on before you logged in.

--- pseudobook/routes/admin.py
+++ pseudobook/routes/admin.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, flash
 from flask import render_template, url_for, request, redirect 
 from flask_login import login_user, logout_user, login_required, current_user
+from urllib.parse import urlparse, urljoin
 
 from pseudobook.database import mysql
 
@@ -17,29 +18,17 @@ mod = Blueprint('admin', __name__, template_folder='../templates/admin')
 '''
 Routes
 '''
-@mod.route('/login', methods=['GET', 'POST'])
+@mod.route('/login', methods=['GET'])
 def login():
-
     # User already logged in
     if not current_user.is_anonymous:
         return redirect(url_for('user_page.user_page', userID=current_user.userID))
-    # Render login page
-    elif request.method == 'GET':
-        login_user_form = LoginUserForm()
-        return render_template('login.html', form=login_user_form)
-    # Validate login
-    else:
-        email = request.form['email']
-        password = request.form['password']
 
-        user = user_model.User.get_user_by_email(email)
-        if user and user.verify_password(password):
-            login_user(user)
-            return redirect(url_for('user_page.user_page', userID=current_user.userID))
-        else:
-            # tell user about failed login
-            flash('Invalid login credentials')
-            return redirect(url_for('admin.login'))
+    # Render login page
+    login_user_form = LoginUserForm()
+    # Get page to redirect back to after successful login, if available
+    next = get_redirect_target()
+    return render_template('login.html', form=login_user_form, next=next)
 
 @mod.route('/logout', methods=['GET'])
 @login_required
@@ -49,37 +38,74 @@ def logout():
 
 @mod.route('/signup', methods=['GET', 'POST'])
 def signup():
-
     # Render signup page
-    if request.method == 'GET':
-        create_user_form = CreateUserForm()
-        return render_template('signup.html', form=create_user_form)
-    # Validate signup
+    create_user_form = CreateUserForm()
+    return render_template('signup.html', form=create_user_form)
+
+'''
+Form Routes
+'''
+@mod.route('/admin/forms/login', methods=['POST'])
+def login_form():
+    email = request.form['email']
+    password = request.form['password']
+    target = request.form['next']
+
+    user = user_model.User.get_user_by_email(email)
+    if user and user.verify_password(password):
+        login_user(user)
+        # Redirect back to refering page if available
+        if target and is_safe_url(target):
+            return redirect(target)
+        else:
+            return redirect(url_for('user_page.user_page', userID=current_user.userID))
     else:
+        # tell user about failed login
+        flash('Invalid login credentials')
+        return redirect(url_for('admin.login'))
 
-        firstName = request.form['firstName']
-        lastName = request.form['lastName']
-        email = request.form['email']
-        password = request.form['password']
-        password_B = request.form['password_B']
+@mod.route('/admin/forms/signup', methods=['POST'])
+def signup_form():
+    firstName = request.form['firstName']
+    lastName = request.form['lastName']
+    email = request.form['email']
+    password = request.form['password']
+    password_B = request.form['password_B']
 
-        create_user = CreateUserForm(request.form)
-        if request.form and create_user.validate_on_submit():
-            if user_model.User.get_user_by_email(email):
-                flash('A user with that email already exists.')
-                return redirect(url_for('admin.signup'))
-            elif password != password_B:
-                flash('The two passwords did not match.')
-                return redirect(url_for('admin.signup'))
+    create_user = CreateUserForm(request.form)
+    if request.form and create_user.validate_on_submit():
+        if user_model.User.get_user_by_email(email):
+            flash('A user with that email already exists.')
+            return redirect(url_for('admin.signup'))
+        elif password != password_B:
+            flash('The two passwords did not match.')
+            return redirect(url_for('admin.signup'))
+        else:
+            new_user = user_model.User(None, firstName, lastName, email, user_model.User.hash_password(password))
+            try:
+                new_user.userID = new_user.register_user()
+            except (mysql.connection.Error, mysql.connection.Warning) as e:
+                print("Exeption of type {} occured: {}".format(type(e), e))
             else:
-                new_user = user_model.User(None, firstName, lastName, email, user_model.User.hash_password(password))
-                try:
-                    new_user.userID = new_user.register_user()
-                except (mysql.connection.Error, mysql.connection.Warning) as e:
-                    print("Exeption of type {} occured: {}".format(type(e), e))
-                else:
-                    login_user(new_user)
-                    return redirect(url_for('user_page.user_page', userID=new_user.userID))
+                login_user(new_user)
+                return redirect(url_for('user_page.user_page', userID=new_user.userID))
 
-        flash('There was an error in account creation. Please try again.')
-        return redirect(url_for('admin.signup'))
+    flash('There was an error in account creation. Please try again.')
+    return redirect(url_for('admin.signup'))
+
+'''
+Helpers
+'''
+# Gets page to redirect back to, specified in url's 'next' parameter, or from referer
+def get_redirect_target():
+    for target in request.values.get('next'), request.referrer:
+        if not target:
+            continue
+        if is_safe_url(target):
+            return target
+
+# Makes sure url is safe & leads back to our server
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
