c6040c684d825241eda08f2d49f516242fa53ece
Edgar Samudio <esamudio@Edgars-MacBook-Pro.local>
2016-12-07 19:33:25 -0500

    changed return values/type

--- pseudobook/models/group.py
+++ pseudobook/models/group.py
@@ -32,22 +32,15 @@ class Group():
 
 
     @staticmethod
-    def group_names(userID):
-
+    def group_list(userID):
         cursor = mysql.connection.cursor()
-        query = '''SELECT G.groupName AS group_name
+        query = '''SELECT G.groupName, G.groupID, G.ownerID
                           FROM GroupUsers GU, `Group` G
                           WHERE GU.userID = {} AND G.groupID = GU.groupID
                           '''.format(userID)
-        # print(query)
         cursor_return = cursor.execute(query)
-        # print(cursor_return)
         results = cursor.fetchall()
-        # print(results)
-        # list_of_names = results.get('list_of_names')
-        list_of_names = ('\n'.join(s['group_name'] for s in results))
-        # print(list_of_names)
-        return list_of_names
+        return [tup for tup in (x for x in results)]
 
 '''
 author @edgar
